0e7929d2590c36544842914da96db34e6e777eb5
Cheng Lin <cheng.lin2003cl@gmail.com>
2024-02-21 19:02:24 +0800

    commit generate input

--- src/GenerateInput.java
+++ src/GenerateInput.java
@@ -1,41 +1,20 @@
+/*
+ * Generate arrays of increasing sizes, in a range from 1,000 to 10 million. 
+ * For each of the sizes, generate a random dataset of integers
+ * in the range of [1, …, x], where x is the largest number you allow for your datasets.
+ */
 import java.util.*;
 
 public class GenerateInput {
+    static final int MIN = 1;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int[] testArr = generateRandom(10, 100);
+        System.out.println(Arrays.toString(testArr));
 
-        System.out.print("Enter the maximum value (x) for the datasets: ");
-        int maxVal = scanner.nextInt();
-        scanner.nextLine(); // Consume newline character
-
-        // Range for array sizes
-        int minSize = 1000;
-        int maxSize = 10000000;
-        int step = 1000000;
-
-        // Generate input data
-        Map<Integer, int[]> inputMap = generateInputData(minSize, maxSize, step, maxVal);
-
-        // Print generated input data
-        for (Map.Entry<Integer, int[]> entry : inputMap.entrySet()) {
-            System.out.println("Size: " + entry.getKey() + ", Data: " + Arrays.toString(entry.getValue()));
-        }
-
-        scanner.close(); // Close the scanner
     }
 
-    public static Map<Integer, int[]> generateInputData(int minSize, int maxSize, int step, int maxVal) {
-        Map<Integer, int[]> inputMap = new LinkedHashMap<>();
-        Random random = new Random();
-
-        for (int size = minSize; size <= maxSize; size += step) {
-            int[] data = new int[size];
-            for (int i = 0; i < size; i++) {
-                data[i] = random.nextInt(maxVal) + 1; // Random integer in range [1, maxVal]
-            }
-            inputMap.put(size, data);
-        }
-
-        return inputMap;
+    public static int[] generateRandom(int size, int max) {
+        return new Random().ints(size, 0, max).toArray();
     }
+
 }
